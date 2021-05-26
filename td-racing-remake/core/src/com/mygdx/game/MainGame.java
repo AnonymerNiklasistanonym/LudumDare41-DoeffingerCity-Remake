ca706ef04c6f598e535c982083f96f2a860da77c
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2021-05-26 06:54:53 +0200

    Add and enforce code style

--- td-racing-remake/core/src/com/mygdx/game/MainGame.java
+++ td-racing-remake/core/src/com/mygdx/game/MainGame.java
@@ -6,51 +6,51 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+    /**
+     * Height of the game screen (the window)
+     */
+    public final static int GAME_HEIGHT = 720;
+    /**
+     * Width of the game screen (the window)
+     */
+    public final static int GAME_WIDTH = 1280;
+    /**
+     * Name of the game
+     */
+    public final static String GAME_NAME = "TnT (Tracks `n Towers)";
+    /**
+     * The provided icon sizes
+     */
+    public final static int[] GAME_ICON_SIZES = {16, 32, 64};
+    SpriteBatch batch;
+    Texture img;
 
-	/**
-	 * Height of the game screen (the window)
-	 */
-	public final static int GAME_HEIGHT = 720;
-	/**
-	 * Width of the game screen (the window)
-	 */
-	public final static int GAME_WIDTH = 1280;
-	/**
-	 * Name of the game
-	 */
-	public final static String GAME_NAME = "TnT (Tracks `n Towers)";
+    /**
+     * Get the filepath of a game icon given its size
+     *
+     * @param IconSize The size of the game icon
+     */
+    public static String getGameIconFilePath(int IconSize) {
+        return "icon/icon_" + IconSize + ".png";
+    }
 
-	/**
-	 * The provided icon sizes
-	 */
-	public final static int[] GAME_ICON_SIZES = { 16, 32, 64 };
-	/**
-	 * Get the filepath of a game icon given its size
-	 * @param IconSize The size of the game icon
-	 */
-	public static String getGameIconFilePath(int IconSize) {
-		return "icon/icon_" + IconSize + ".png";
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    @Override
+    public void render() {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 }
--- td-racing-remake/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ td-racing-remake/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -6,22 +6,23 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MainGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		// set window width and height
-		config.height = MainGame.GAME_HEIGHT;
-		config.width = MainGame.GAME_WIDTH;
+  public static void main(String[] arg) {
+    LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-		// set window title
-		config.title = MainGame.GAME_NAME;
-		config.fullscreen = false;
+    // set window width and height
+    config.height = MainGame.GAME_HEIGHT;
+    config.width = MainGame.GAME_WIDTH;
 
-		// set window icon
-		for (int size : MainGame.GAME_ICON_SIZES) {
-			config.addIcon(MainGame.getGameIconFilePath(size), Files.FileType.Internal);
-		}
+    // set window title
+    config.title = MainGame.GAME_NAME;
+    config.fullscreen = false;
 
-		new LwjglApplication(new MainGame(), config);
-	}
+    // set window icon
+    for (int size : MainGame.GAME_ICON_SIZES) {
+      config.addIcon(MainGame.getGameIconFilePath(size), Files.FileType.Internal);
+    }
+
+    new LwjglApplication(new MainGame(), config);
+  }
 }
